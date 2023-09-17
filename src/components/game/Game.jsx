35cdbc0ba4e206550ddc3c288ac789bbabf62b15
Denner-Levi <denner.c.filho@gmail.com>
2023-09-17 16:57:46 -0300

    Criado lógica vencedor

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,54 @@
 import Styles from "./game.module.css"
 import GameOption from "../gameOption/gameOption"
-import { useState } from "react"
+import { useState, useEffect  } from "react"
 import Icon from "../icon/Icon";
+
+
+const WinnerTable = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 function Game () {
     let varcss = "gameinfo";
     const [gameState, SetgameState] = useState(Array(9).fill(0));
     const [currentPlayer, SetCurrentPlayer] = useState(1)
+    const [Winner, SetWinner] = useState(0)
+
+
     const handleCLick = (pos) => {
-        if (gameState[pos] === 0) {
+        if (gameState[pos] === 0 && Winner === 0) {
             let NewGameState = [...gameState]
             NewGameState[pos] = currentPlayer
-            SetCurrentPlayer(currentPlayer * -1)
             SetgameState(NewGameState)
         }
     }
     if (currentPlayer === 1) {
         varcss = 'gameinfo2';
     }
+
+    const verifyGame = () => {
+        WinnerTable.forEach( (line) => {
+            const values =  line.map((pos) => gameState[pos])
+            const sum = values.reduce((sum, value) => sum + value)
+            if (sum === 3 || sum === -3) {
+            SetWinner(sum / 3)
+            }
+        } )
+    }
+
+    useEffect( () => {
+        SetCurrentPlayer(currentPlayer * -1)
+        verifyGame()
+    }, [gameState] )
+
+
     return(
         <div className={Styles.gameContent}>
 
