0568007647f4b28861a67e4c5ec9bd22aa758666
Denner-Levi <denner.c.filho@gmail.com>
2023-09-17 11:40:19 -0300

    Criado States na gameOption

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,18 +1,21 @@
 import Styles from "./game.module.css"
 import GameOption from "../gameOption/gameOption"
+import { useState } from "react"
 
 function Game () {
+    const [gameState, SetgameState] = useState(Array(9).fill(0));
     return(
         <div className={Styles.game}>
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
+            {
+                gameState.map((value, pos) => 
+                <GameOption 
+                
+                key={`game-option-pos-${pos}`}
+                status={value}
+
+                />)
+            }
+
         </div>
     )
 }
--- src/components/gameOption/gameOption.jsx
+++ src/components/gameOption/gameOption.jsx
@@ -1,11 +1,18 @@
 import Styles from "./gameOption.module.css"
+import Icon from '../icon/Icon'
 
 
 
-function GameOption () {
+function GameOption ( { status } ) {
     return(
         <div className={Styles.gameoption}>
+            {
+                status === 1 && <Icon iconName="circle" size="25px" />
+            }
             
+            {
+                status === -1 && <Icon iconName="x" size="25px" />
+            }            
         </div>
     )
 }
