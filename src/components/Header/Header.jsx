3ce7571417266234bccd21c8713a1bd32ab328c1
Denner-Levi <denner.c.filho@gmail.com>
2023-09-17 10:42:19 -0300

    Criado icone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import Title from "../Title/Title"
 import Subtitle from "../Subtitle/Subtitle"
 import Styles from './Header.module.css'
+import Icon from "../icon/Icon"
 
 function Header () {
     return(
         <div className={Styles.header}>
             <Title>Jogo Da Velha</Title>
             <Subtitle>Por: Denner Levi</Subtitle>
+            <div className={Styles.iconContent}>
+                <Icon iconName="github" link="https://github.com/Denner-Levi" />
+            </div>
         </div>
     )
 }
